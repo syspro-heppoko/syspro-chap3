78acd49be87484aa6241ebfe7f5219a65e703d50
syspro-heppoko <east.snow.yokohama205@gmail.com>
2020-08-20 12:45:53 +0100

    add led blink code

---
+++ ledblink.py
@@ -0,0 +1,14 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(14, GPIO.OUT)
+
+while 1:
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(1)
+
+GPIO.cleanup()
